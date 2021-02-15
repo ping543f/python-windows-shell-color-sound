dc59db26292d9d9cc3d1762e76a0afc9e09a3d4d
Ping543f <Ping543f@gmail.com>
2021-02-16 07:36:21 +0800

    Create cbshell.py

---
+++ cbshell.py
@@ -0,0 +1,24 @@
+from numpy import random
+import os
+import winsound
+
+## Frequency and duration for the beep sound
+frequency = 2000  # Set Frequency To 2000 Hertz
+duration = 100  # Set Duration // 1000 ms == 1 second
+
+##generating random values from 1 to 9, where each number represents a foreground color of the windows command prompt or shell
+
+c = random.choice([1,2,3,4, 5,6, 7,8, 9])
+color_string = "color "
+
+## This will generate a list of random floating numbers of 5 items for infinite time
+while True:
+    c = random.choice([1,2,3,4, 5,6, 7,8, 9])
+    x = random.rand(1,5)
+    color_s = color_string+str(c)
+    os.system(color_s)
+    print(x)
+    os.system(color_s)
+    print('\a') ## this is unnecessary but I kept it for no reason
+    winsound.Beep(frequency, duration)
+    
